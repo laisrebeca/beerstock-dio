796302167d920c633507db92abdbbae68ad6b706
Laís Rebeca <laisrebeca26@gmail.com>
2021-09-21 14:49:17 -0300

    Primeiro Commit

---
+++ src/test/java/beerstockdesafiodio/beerstockdio/BeerstockDioApplicationTests.java
@@ -0,0 +1,13 @@
+package beerstockdesafiodio.beerstockdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
